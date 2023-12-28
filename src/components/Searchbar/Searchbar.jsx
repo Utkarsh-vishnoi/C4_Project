2d647e2b3126cabf500dafe906108d59484f5c8d
hemachandran66 <115919725+hemachandran66@users.noreply.github.com>
2023-12-28 23:59:36 +0530

    searchbar fix

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,65 +1,37 @@
 import { Search } from "@mui/icons-material";
-import { TextField, alpha, styled } from "@mui/material";
+import { alpha, OutlinedInput, InputAdornment } from "@mui/material";
 import React, { useState } from "react";
 
 const Searchbar = () => {
   const [query, setQuery] = useState('');
-
-  const SearchWrapper = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(20),
-      width: "auto",
-    },
-  }));
-
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
-
-  const StyledInputBase = styled(TextField)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("md")]: {
-        width: "30ch",
-      },
-    },
-  }));
-
   return (
-    <SearchWrapper>
-      <SearchIconWrapper>
-        <Search />
-      </SearchIconWrapper>
-      <StyledInputBase
-        id="search-bar"
-        placeholder="Search…"
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        inputProps={{ "aria-label": "search" }}
-      />
-
-    </SearchWrapper>
+    <div style={{width: "33%", textAlign: "center", margin: "0 auto"}}>
+    <OutlinedInput
+    id="search"
+    sx={{
+      flexGrow: 1,
+      color: "#FFFFFF",
+      borderRadius: 1,
+      backgroundColor: (theme) => alpha(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: (theme) => alpha(theme.palette.common.white, 0.25),
+      },
+      width: "100%",
+      marginLeft: 20,
+    }}
+    value={query}
+    onChange={(e) => {setQuery(e.target.value)}}
+    startAdornment={
+      <InputAdornment position="start">
+        <Search sx={{color: "#FFFFFF"}}/>
+      </InputAdornment>
+    }
+    placeholder={"Search"}
+    inputProps={{
+      'aria-label': 'search',
+    }}
+  />
+  </div>
   );
 };
 
